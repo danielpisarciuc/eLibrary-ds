032d6be1b0af87442aecf96217cf17d65944775c
Ionut <ionutdny9@gmail.com>
2016-05-15 20:17:48 +0300

    [IP] -  book search client

--- elibrary-client/src/main/java/com/elibrary/client/LibraryClient.java
+++ elibrary-client/src/main/java/com/elibrary/client/LibraryClient.java
@@ -1,8 +1,14 @@
 package com.elibrary.client;
 
+import com.elibrary.common.dto.SearchBookDto;
+
 import javax.ws.rs.core.Response;
 
 public interface LibraryClient {
 
-    Response getReferenceData(String type);
+    Response referenceData(String type);
+
+    Response bookSimpleSearch(String searchTerm);
+
+    Response bookComplexSearch(SearchBookDto searchBook);
 }
--- elibrary-client/src/main/java/com/elibrary/client/impl/LibraryClientImpl.java
+++ elibrary-client/src/main/java/com/elibrary/client/impl/LibraryClientImpl.java
@@ -1,10 +1,12 @@
 package com.elibrary.client.impl;
 
 import com.elibrary.client.LibraryClient;
+import com.elibrary.common.dto.SearchBookDto;
 import org.glassfish.jersey.client.ClientConfig;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.Response;
 
@@ -13,18 +15,41 @@ public class LibraryClientImpl implements LibraryClient {
     private String eLibraryUrl;
     private Client client;
     private String referenceData;
+    private String simpleSearch;
+    private String complexSearch;
 
     public LibraryClientImpl(String eLibraryUrl) {
         this.eLibraryUrl = eLibraryUrl;
         this.referenceData = this.eLibraryUrl + "/" + "reference-data?type=";
+        this.simpleSearch = this.eLibraryUrl + "/" + "simple-search?searchTerm=";
+        this.complexSearch = this.eLibraryUrl + "/" + "complex-search";
     }
 
     @Override
-    public Response getReferenceData(String type) {
+    public Response referenceData(String type) {
         client = ClientBuilder.newClient(new ClientConfig().register(Response.class));
         WebTarget target = client.target(this.referenceData + type);
         Response response = target.request().get();
 
         return response;
     }
+
+    @Override
+    public Response bookSimpleSearch(String searchTerm) {
+        client = ClientBuilder.newClient(new ClientConfig().register(Response.class));
+        WebTarget target = client.target(this.simpleSearch + searchTerm);
+        Response response = target.request().get();
+
+        return response;
+    }
+
+    @Override
+    public Response bookComplexSearch(SearchBookDto searchBook) {
+        client = ClientBuilder.newClient(new ClientConfig().register(Response.class));
+        WebTarget target = client.target(this.complexSearch);
+        Response response = target.request().post(Entity.json(searchBook));
+
+        return response;
+    }
+
 }
