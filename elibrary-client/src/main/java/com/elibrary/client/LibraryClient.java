0f2f63d1991c7606c0e1679f03088a9be529637b
Ionut <ionutdny9@gmail.com>
2016-05-15 19:37:24 +0300

    [IP] - implement the reference data client

--- elibrary-client/src/main/java/com/elibrary/client/LibraryClient.java
+++ elibrary-client/src/main/java/com/elibrary/client/LibraryClient.java
@@ -1,4 +1,8 @@
 package com.elibrary.client;
 
+import javax.ws.rs.core.Response;
+
 public interface LibraryClient {
+
+    Response getReferenceData(String type);
 }
--- elibrary-client/src/main/java/com/elibrary/client/impl/LibraryClientImpl.java
+++ elibrary-client/src/main/java/com/elibrary/client/impl/LibraryClientImpl.java
@@ -1,15 +1,30 @@
 package com.elibrary.client.impl;
 
 import com.elibrary.client.LibraryClient;
+import org.glassfish.jersey.client.ClientConfig;
 
 import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
 
 public class LibraryClientImpl implements LibraryClient {
 
     private String eLibraryUrl;
     private Client client;
+    private String referenceData;
 
     public LibraryClientImpl(String eLibraryUrl) {
         this.eLibraryUrl = eLibraryUrl;
+        this.referenceData = this.eLibraryUrl + "/" + "reference-data?type=";
+    }
+
+    @Override
+    public Response getReferenceData(String type) {
+        client = ClientBuilder.newClient(new ClientConfig().register(Response.class));
+        WebTarget target = client.target(this.referenceData + type);
+        Response response = target.request().get();
+
+        return response;
     }
 }
