669a391936a4f4410000964919f495d29b96a3ae
Ionut <ionutdny9@gmail.com>
2016-05-22 14:53:46 +0300

    [IP] - application info endpoint

---
+++ elibrary-server/src/main/java/com/elibrary/server/controller/ApplicationInfo.java
@@ -0,0 +1,24 @@
+package com.elibrary.server.controller;
+
+
+import com.elibrary.server.utils.LibraryMessage;
+import org.springframework.stereotype.Controller;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/app")
+@Controller
+public class ApplicationInfo {
+
+    @GET
+    @Path("/info")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response verifyRESTService() {
+        return Response.status(200).entity(LibraryMessage.APPLICATION_INFO.getMessage()).build();
+    }
+}
+
