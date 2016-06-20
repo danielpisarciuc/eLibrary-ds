6d33f674c1c3afbc5ac25d53d336cec6dbef1294
Ionut <ionutdny9@gmail.com>
2016-06-20 22:45:19 +0300

    [IP] - spring security mock

---
+++ elibrary-server/src/main/java/com/elibrary/server/security/SpringSecurityInitializer.java
@@ -0,0 +1,21 @@
+package com.elibrary.server.security;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+//
+//public class SpringSecurityInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+//
+//    @Override
+//    protected Class<?>[] getRootConfigClasses() {
+//        return new Class[] { SecurityConfig.class };
+//    }
+//
+//    @Override
+//    protected Class<?>[] getServletConfigClasses() {
+//        return null;
+//    }
+//
+//    @Override
+//    protected String[] getServletMappings() {
+//        return new String[] { "/" };
+//    }
+//}
